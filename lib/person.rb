071b63ab9b2d33bb9a5f6be1cd2b658300f95438
delaneybarrow <delaneybarrow19@gmail.com>
2018-06-08 17:28:14 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,15 @@
 # person.rb
+class Person 
+  
+  def talk
+    puts "Hello World!"
+  end
+  
+  def walk
+    puts "The Person is walking"
+  end
+  
+end
+delaney = Person.new 
+delaney.talk
+delaney.walk
